2dbeeb3d4dc3dd1cb9d7c5f58f16f5b64e421639
ISCOM - JoshJhan <joshjhan@iscom.com.tw>
2022-03-24 16:48:37 +0800

    newProject

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <!-- 側攔區塊 -->
+    <asideMenu />
+
+    <!-- 地圖區塊 -->
+    <div class="mask-map" id="mask-map"></div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+import asideMenu from "./components/asideMenu";
+export default {
+  name: "App",
+  components: { asideMenu },
+  methods: {
+    ...mapActions(["fetchLocations", "fetchPharmacies"]),
+  },
+  mounted() {
+    this.fetchLocations();
+    this.fetchPharmacies();
+  },
+};
+</script>
+
+<style lang="scss" src="./style.scss"></style>
